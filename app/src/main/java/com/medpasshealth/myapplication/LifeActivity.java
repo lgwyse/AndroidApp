5987d241f29bad09133e6ec6f332e2c086545922
lwyse13 <lgwyse@crimson.ua.edu>
2015-02-05 13:43:31 -0600

    login screen

--- app/src/main/java/com/medpasshealth/myapplication/LifeActivity.java
+++ app/src/main/java/com/medpasshealth/myapplication/LifeActivity.java
@@ -1,12 +1,12 @@
 package com.medpasshealth.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class LifeActivity extends ActionBarActivity {
+public class LifeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
