59d56d1b210ff315cc9a13cc21c15a855775bbe7
lwyse13 <lgwyse@crimson.ua.edu>
2015-02-10 13:12:17 -0600

    register proto-layouts

--- app/src/main/java/com/medpasshealth/myapplication/VerifyIdentity.java
+++ app/src/main/java/com/medpasshealth/myapplication/VerifyIdentity.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class AccountVerify extends Activity {
+public class VerifyIdentity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account_verify);
+        setContentView(R.layout.activity_verify_identity);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_account_verify, menu);
+        getMenuInflater().inflate(R.menu.menu_verify_identity, menu);
         return true;
     }
 
