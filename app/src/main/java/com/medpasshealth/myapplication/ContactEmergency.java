31d54c94fe698c5d1aa54960dd15a71e7f31a727
lwyse13 <lgwyse@crimson.ua.edu>
2015-02-23 14:19:04 -0600

    registration validation

--- app/src/main/java/com/medpasshealth/myapplication/ContactEmergency.java
+++ app/src/main/java/com/medpasshealth/myapplication/ContactEmergency.java
@@ -7,7 +7,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class ContactEmergency extends Activity {
@@ -17,17 +19,42 @@ public class ContactEmergency extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_emergency);
 
-        findViewById(R.id.contnu).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ContactEmergency.this.startActivity(new Intent(ContactEmergency.this, EnteringSemester.class));
-            }
-        });
+        final EditText firstname = (EditText) findViewById(R.id.firstname);
+        final EditText lastname = (EditText) findViewById(R.id.lastname);
+        final TextView relationtext = (TextView) findViewById(R.id.relationtext);
+        final EditText primaryphone = (EditText) findViewById(R.id.primaryphone);
+        final EditText secphone = (EditText) findViewById(R.id.secphone);
 
-        Spinner spinner = (Spinner) findViewById(R.id.relationship);
+        final Spinner relationship = (Spinner) findViewById(R.id.relationship);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.relation_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        relationship.setAdapter(adapter);
+        
+        findViewById(R.id.contnu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relationtext.setError(null);
+                
+                if( firstname.getText().toString().trim().length() == 0 ) {
+                    firstname.setError("Required.");
+                }
+                else if( lastname.getText().toString().trim().length() == 0 ) {
+                    lastname.setError("Required.");
+                }
+                else if( relationship.getSelectedItemPosition()==0) {
+                    relationtext.setError("Required.");
+                }
+                else if( primaryphone.getText().toString().trim().length() == 0 ) {
+                    primaryphone.setError("Required.");
+                }
+                else if( secphone.getText().toString().trim().length() == 0 ) {
+                    secphone.setError("Required.");
+                }
+                else ContactEmergency.this.startActivity(new Intent(ContactEmergency.this, EnteringSemester.class));
+            }
+        });
+
+        
     }
 }
--- app/src/main/java/com/medpasshealth/myapplication/Register.java
+++ app/src/main/java/com/medpasshealth/myapplication/Register.java
@@ -8,21 +8,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
 
 public class Register extends Activity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        final EditText username = (EditText) findViewById(R.id.email);
+        final EditText newpassword = (EditText) findViewById(R.id.newpassword);
+        final EditText confirmnewpass = (EditText) findViewById(R.id.confirmpassword);
+        final Switch agreementswitch = (Switch) findViewById(R.id.agreementswitch);
+
         findViewById(R.id.agreecbtext).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                
                 Register.this.startActivity(new Intent(Register.this, TermsOfUse.class));
             }
         });
@@ -30,10 +36,24 @@ public class Register extends Activity {
         findViewById(R.id.registerbutton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (((Switch) findViewById(R.id.agreementswitch)).isChecked()) {
-                    Register.this.startActivity(new Intent(Register.this, CreatePassword.class));
-                    }
+
+                if( username.getText().toString().trim().length() == 0 ) {
+                    username.setError("An email address is required.");
                 }
+                else if( newpassword.getText().toString().trim().length() == 0 ) {
+                    newpassword.setError("A password is required.");
+                }
+                else if( confirmnewpass.getText().toString().trim().length() == 0 ) {
+                    confirmnewpass.setError("Please confirm your password.");
+                }
+                else if (!newpassword.getText().toString().equals(confirmnewpass.getText().toString())) {
+                    confirmnewpass.setError("Password fields must match.");
+                }
+                else if (!agreementswitch.isChecked()) {
+                    agreementswitch.setError("Must accept agreement to register.");
+                }
+                else Register.this.startActivity(new Intent(Register.this, ContactPersonal.class));
+            }
         });
     }
 
